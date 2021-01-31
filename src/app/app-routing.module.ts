10e1e881075edac15b1f522754c6dcd9b17c781e
nayanamanoharan <nayanapm001@gmail.com>
2021-01-31 19:42:55 +0530

    Added navigation for home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { LogInComponent } from './components/log-in/log-in.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: 'login', component: LogInComponent },
+  { path: 'login',component: LogInComponent },
+  { path: 'home',component: HomeComponent},
 ];
 
 @NgModule({
--- src/app/components/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  public loginInvalid: boolean;
+  private returnUrl: string;
+  constructor(
+    private router: Router,
+    private fb: FormBuilder
+    ) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.email],
+      password: ['', Validators.required]
+    });
   }
 
+  onSubmit() {
+    var defUsername:string = "nayanapm@gmail.com"
+    var defPassword:string = "pwd@123"
+    this.loginInvalid = false;
+    if (this.form.valid) {
+      const username = this.form.get('username').value;
+      const password = this.form.get('password').value;
+      if (username == defUsername && password == defPassword) {
+        this.router.navigateByUrl("/home")
+      }
+    }
+  }
 }
